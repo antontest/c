7a147f4c8ca5101fbb021108ad713480212a538f
anton <d198908@163.com>
2016-01-14 19:39:07 +0800

    add app url to get html or certs

--- class/url/src/main/url.c
+++ class/url/src/main/url.c
@@ -1,11 +1,11 @@
-/**
- * usual head files
- */
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <url.h>
+#include <utils/get_args.h>
+#include <utils/utils.h>
 
 /*********************************************************
  **************    Function Declaration    ***************
@@ -17,7 +17,82 @@
 int main(int agrc, char *agrv[])
 {
     int rt = 0; /* return value of function main */
-    url_t *url = url_create();
+    int help_flag  = 0;
+    int ejbca_flag = 0;
+    int post_flag  = 0;
+    int https_flag = 0;
+    char *url      = NULL;
+    char *ssl_info = NULL;
+    char *ssl_cert = NULL;
+    char *ssl_key  = NULL;
+    char *user     = NULL;
+    char *password = NULL;
+    char *data     = NULL;
+    char http_url[512] = {0};
+    char http_htm[128] = {0};
+    struct options opt[] = {
+        {"-r", "--url",      1, RET_STR, ADDR_ADDR(url)},
+        {"-i", "--sslinfo",  1, RET_STR, ADDR_ADDR(ssl_info)},
+        {"-c", "--sslcert",  1, RET_STR, ADDR_ADDR(ssl_cert)},
+        {"-k", "--sslkey",   1, RET_STR, ADDR_ADDR(ssl_key)},
+        {"-u", "--user",     1, RET_STR, ADDR_ADDR(user)},
+        {"-p", "--password", 1, RET_STR, ADDR_ADDR(password)},
+        {"-e", "--ejbca",    0, RET_INT, ADDR_ADDR(ejbca_flag)},
+        {"-d", "--data",     1, RET_STR, ADDR_ADDR(data)},
+        {NULL, "--post",     0, RET_INT, ADDR_ADDR(post_flag)},
+        {"-h", "--help",     0, RET_INT, ADDR_ADDR(help_flag)},
+        {NULL},
+    };
+    struct usage usg[] = {
+        {"-r, --url",      "http or https url"},
+        {"-i, --sslinfo",  "ssl ca cert path"},
+        {"-c, --sslcert",  "ssl client cert"},
+        {"-k, --sslkey",   "ssl client key"},
+        {"-u, --user",     "user name"},
+        {"-p, --password", "user password"},
+        {"-e, --ejbca",    "download cert from ejbca server"},
+        {"-d, --data",     "post data"},
+        {"-h, --help",     "show usage"},
+        {NULL},
+    };
+    url_t *curl = NULL;
+
+    /**
+     * parse cmdline
+     */
+    get_args(agrc, agrv, opt);
+    if (help_flag) {
+        print_usage(usg);
+        return 0;
+    }
+    if (!url && !ejbca_flag) return -1;
+    if (ejbca_flag && (!user || !password)) return -1;
+    if (!url) url = "https://172.21.34.86:8443/ejbca/adminweb/ra/editendentity.jsp";
+    if (!ssl_info) ssl_info = "/home/anton/certs/ejbca/superadmin/ssl.ca.crt.pem";
+    if (!ssl_cert) ssl_cert = "/home/anton/certs/ejbca/superadmin/ssl.cli.crt.pem";
+    if (!ssl_key)  ssl_key  = "/home/anton/certs/ejbca/superadmin/ssl.cli.key.pem";
+
+    curl = url_create();
+    if (!curl) return -1;
+
+    https_flag = strcasestr(url, "https") ? 1: 0;
+    if (https_flag) curl->ssl_init(curl, ssl_info, ssl_cert, ssl_key);
+    if (ejbca_flag) {
+        snprintf(http_url, sizeof(http_url), "%s?username=%s", url, user);
+        snprintf(http_htm, sizeof(http_htm), "%s.htm", user);
+    } else {
+        snprintf(http_url, sizeof(http_url), "%s", url);
+        snprintf(http_htm, sizeof(http_htm), "tmp.htm");
+    }
+    curl->set_url(curl, http_url, http_htm);
+    
+    if (!post_flag) curl->get(curl);
+    else {
+        if (!data) return -1;
+        curl->post(curl, data);
+    }
+    curl->parse_form_data(curl);
+    curl->list_data(curl);
 
     /*
     url->set_url(url, "http://172.21.34.27:8000", "index.htm");
@@ -44,21 +119,6 @@ int main(int agrc, char *agrv[])
     url->form_post(url);
     */
 
-    url->ssl_init(url, 
-        "/home/anton/certs/ejbca/superadmin/ssl.ca.crt.pem",
-        "/home/anton/certs/ejbca/superadmin/ssl.cli.crt.pem",
-        "/home/anton/certs/ejbca/superadmin/ssl.cli.key.pem"
-    );
-    url->set_url(url, "https://172.21.34.86:8443/ejbca/adminweb/ra/editendentity.jsp?username=anton", "anton.htm");
-    url->get(url);
-    url->parse_form_data(url);
-    url->list_data(url);
-    
-    char *value = NULL;
-    url->set_value(url, "selecttoken", "654321");
-    value = url->get_value(url, "selecttoken");
-    printf("get_value: %s\n", value);
-
-    url->destroy(url);
+    curl->destroy(curl);
     return rt;
 }
