d462b3e1cf0d4fba5a276a2c0e4d5e79ffd96245
anton <d198908@163.com>
2016-04-27 17:15:59 +0800

    test app: add test for xls

---
+++ makefile/test/xls/xls.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <xls/xls.h>
+#include <locale.h>
+#include <iconv.h>
+
+int main(int argc, char **argv)
+{
+    xls_t *xls = NULL;
+
+    xls = xls_create();
+    xls->open(xls, "tt.xls", O_WRONLY);
+    xls->open_sheet(xls, 3);
+    xls->write(xls, 3, 0, "测试1");
+    xls->save(xls, "tt.xls");
+
+    /**
+     * for read
+     */
+    /*
+    char *str;
+    while ((str = xls->enumerate(xls))) {
+        printf("%s ", str);
+    }
+    */
+    xls->destroy(xls);
+    
+    return 0;
+}
