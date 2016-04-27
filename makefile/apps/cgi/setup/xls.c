3d9349c8fbd39a2a22cd1c96e1f73a57eaafa642
anton <d198908@163.com>
2016-04-27 19:44:59 +0800

    apps cgi: add xls read

---
+++ makefile/apps/cgi/setup/xls.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cgi/cgi.h>
+#include <xls/xls.h>
+
+int xls_data(char *outbuf, char *errbuf, cgi_form_entry_t *entry)
+{
+    char *xls_file = NULL;
+    xls_t *xls     = NULL;
+    int col, row;
+    char *str;
+
+    xls = xls_create();
+    if (!xls) {
+        ALERT("内存溢出!");
+        return -1;
+    }
+
+    if (xls->open(xls, xls_file, O_RDONLY) < 0) {
+        ALERT("xls文档不存在!");
+        return -1;
+    }
+
+    for (row = 3; row < xls->get_row_cnt(xls); row++) {
+        printf("<tr>");
+        for (col = 0; col < 15; col++) {
+            printf("<td>");
+            str = xls->read(xls, row, col);
+            if (str) {
+                printf("%s", str);
+            } else {
+                printf(" ");
+            }
+            printf("<\td>");
+
+        }
+        printf("<\tr>");
+    }
+
+    xls->destroy(xls);
+    
+    return 0;
+}
---
+++ makefile/apps/cgi/setup/xls.h
@@ -0,0 +1,6 @@
+#ifndef __XLS_H__
+#define __XLS_H__
+
+int xls_data(char *outbuf, char *errbuf, cgi_form_entry_t *entry);
+
+#endif /* __XLS_H__ */
