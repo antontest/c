ae0efe0a7fb591984bba98126615dc9773ade257
Antonio <antonlee2013@163.com>
2016-08-31 22:12:03 +0800

    class utils: add enum status.

--- makefile/library/utils/utils/utils.h
+++ makefile/library/utils/utils/utils.h
@@ -32,6 +32,71 @@ typedef unsigned short int u16 ;
 typedef int s32 ;
 typedef unsigned int u32 ;
 typedef enum status_t status_t;
+/**
+ * Return values of function calls.
+ */
+enum status_t {
+	/**
+	 * Call succeeded.
+	 */
+	SUCCESS,
+
+	/**
+	 * Call failed.
+	 */
+	FAILED,
+
+	/**
+	 * Out of resources.
+	 */
+	OUT_OF_RES,
+
+	/**
+	 * The suggested operation is already done
+	 */
+	ALREADY_DONE,
+
+	/**
+	 * Not supported.
+	 */
+	NOT_SUPPORTED,
+
+	/**
+	 * One of the arguments is invalid.
+	 */
+	INVALID_ARG,
+
+	/**
+	 * Something could not be found.
+	 */
+	NOT_FOUND,
+
+	/**
+	 * Error while parsing.
+	 */
+	PARSE_ERROR,
+
+	/**
+	 * Error while verifying.
+	 */
+	VERIFY_ERROR,
+
+	/**
+	 * Object in invalid state.
+	 */
+	INVALID_STATE,
+
+	/**
+	 * Destroy object which called method beints to.
+	 */
+	DESTROY_ME,
+
+	/**
+	 * Another call to the method is required.
+	 */
+	NEED_MORE,
+};
+
 
 #ifndef _WIN32
 #define SNPRINTF(buf, size, fmt, ...) \
